ff4514921119f2e43d53de978eb2badf6aeda773
Monstrola <116669471+Monstrola@users.noreply.github.com>
2022-11-13 12:43:08 -0700

    Add files via upload

---
+++ Tires.java
@@ -0,0 +1,45 @@
+package finalProject_1410;
+
+public class Tires {
+
+	private double offRoadSpeed;
+	private double roadSpeed;
+	TireType tires;
+	
+	
+	public Tires(TireType tires) {
+		this.tires = tires; 
+		switch (tires) {
+		
+		case LOWPROFILE:
+			offRoadSpeed=100.0;
+			roadSpeed=-200.0;
+			break;
+		case ALL_TERRAIN:
+			offRoadSpeed=150.0;
+			roadSpeed=150.0;
+			break;
+			
+		case OFFROAD:
+			offRoadSpeed=200.0;
+			roadSpeed=100.0;
+			break;	
+			
+		default:
+		offRoadSpeed=0;
+		roadSpeed=0;
+		break;
+		}
+	}
+
+
+	public double getOffRoadSpeed() {
+		return offRoadSpeed;
+	}
+
+
+	public double getRoadSpeed() {
+		return roadSpeed;
+	}
+
+}
