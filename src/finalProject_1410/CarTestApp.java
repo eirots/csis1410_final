56554ac8226bc16f3f0b2c8002e5e239d7936ef4
SpencerPeck <speck34@bruinmail.slcc.edu>
2022-12-04 20:35:26 -0700

    File I/O Added
    Added save/load functions to car and provided testing and examples to the test app.

--- src/finalProject_1410/CarTestApp.java
+++ src/finalProject_1410/CarTestApp.java
@@ -1,11 +1,16 @@
 package finalProject_1410;
 
+import java.io.File;
+
 public class CarTestApp {
 
 	public static void main(String[] args) {
-		Car myCar = new Car(new Body(Frame.SPORTSCAR), new Engine(EngineBlock.VEngine), new Tires(TireType.ALL_TERRAIN));
+		Car myCar = new Car(new Body(Frame.OFFROAD), new Engine(EngineBlock.VEngine), new Tires(TireType.ALL_TERRAIN));
 		
-		System.out.print(myCar.getCurrentSpeed());
+		System.out.println(myCar.getCurrentSpeed());
+		
+		//Car.saveCar(myCar, new File("SavedCars.txt"));
+		System.out.println(Car.loadCar(new File("SavedCars.txt"), 2));
 
 	}
 
